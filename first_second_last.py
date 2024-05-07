3dc1e03fe98e3f8ddabd3d92a296f3fbb2d9954f
Amandeep Kaur <akaur.15544@gmail.com>
2024-05-07 12:09:10 +0300

    Python another exercise

---
+++ first_second_last.py
@@ -0,0 +1,11 @@
+def first_word(sentence):
+ return sentence.split()[0]
+def second_word(sentence):
+  return sentence.split()[1]
+def last_word(sentence):
+  return sentence.split()[-1]
+if __name__ == "__main__":
+    sentence = "once upon a time there was a programmer"
+    print(first_word(sentence))
+    print(second_word(sentence))
+    print(last_word(sentence))
